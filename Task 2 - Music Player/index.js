1d61171b59aa94613601f572c063b6d44173da7f
Muhammad Roshik <130252511+RoshiKK@users.noreply.github.com>
2024-09-20 22:23:40 +0500

    Add files via upload

---
+++ Task 2 - Music Player/index.js
@@ -0,0 +1,43 @@
+let progress = document.getElementById("progress");
+let song = document.getElementById("song");
+let ctrlicon = document.getElementById("ctrlicon");
+let volumeSlider = document.getElementById("volume");
+
+song.volume = 1;
+
+volumeSlider.addEventListener('input',function() {
+    song.volume = volumeSlider.value;
+});
+
+song.onloadedmetadata = function(){
+    progress.max = song.duration;
+    progress.value = song.currentTime;
+
+}
+
+function playPause(){
+    if(ctrlicon.classList.contains("fa-pause")){
+        song.pause();
+        ctrlicon.classList.remove("fa-pause");
+        ctrlicon.classList.add("fa-play");
+    }
+    else{
+        song.play();
+        ctrlicon.classList.remove("fa-play");
+        ctrlicon.classList.add("fa-pause");
+    }
+}
+
+if(song.play()){
+    setInterval(()=>{
+        progress.value= song.currentTime;
+    },500);
+}
+
+progress.onchange = function(){
+    song.play();
+    song.currentTime = progress.value;
+    ctrlicon.classList.add("fa-pause");
+    ctrlicon.classList.remove("fa-play");
+}
+
